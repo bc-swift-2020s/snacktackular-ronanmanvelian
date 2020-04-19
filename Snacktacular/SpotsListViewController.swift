5c4771eb4071bd5d6d9e7e5599cd8fa6de997105
Ronan Manvelian <manvelia@bc.edu>
2020-04-18 21:03:22 -0700

    Completed 9.10

--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -20,6 +20,8 @@ class SpotsListViewController: UIViewController {
     
     var spots: Spots!
     var authUI: FUIAuth!
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,11 +33,11 @@ class SpotsListViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isHidden = true
-        
         spots = Spots()
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        getLocation()
         spots.loadData {
             self.sortBasedOnSegmentPressed()
             self.tableView.reloadData()
@@ -59,6 +61,13 @@ class SpotsListViewController: UIViewController {
         } else {
             tableView.isHidden = false
         }
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -88,12 +97,13 @@ class SpotsListViewController: UIViewController {
         case 0: // A-Z
             spots.spotArray.sort(by: {$0.name < $1.name})
         case 1: // Closest
-            print("TODO")
+            spots.spotArray.sort(by: {$0.location.distance(from: currentLocation) < $1.location.distance(from: currentLocation)})
         case 2: // Avg. Rating
             print("TODO")
         default:
             print("*** ERROR: Hey, you shouldn't have gotten here, our segmented control should just have 3 segments")
         }
+        tableView.reloadData()
     }
     
     @IBAction func sortSegmentPressed(_ sender: UISegmentedControl) {
@@ -120,7 +130,10 @@ extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotsTableViewCell
-        cell.nameLabel.text = spots.spotArray[indexPath.row].name
+        if let currentLocation = currentLocation {
+            cell.currentLocation = currentLocation
+        }
+        cell.configureCell(spot: spots.spotArray[indexPath.row])
         return cell
         
         // Note: we use "as!" in the initialization of "cell" because the cell type
@@ -176,3 +189,54 @@ extension SpotsListViewController: FUIAuthDelegate {
         return loginViewController
     }
 }
+
+extension SpotsListViewController: CLLocationManagerDelegate {
+    
+    func getLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func handleLocationAuthorizationStatus(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .denied:
+            showAlertToPrivacySettings(title: "User has not authorized location services", message: "Select 'settings'below to open device settings and enable location services for this app.")
+        case .restricted:
+            showAlert(title: "Location services denied", message: "It may be that parental controls are restricting location use inthis app")
+        }
+    }
+    
+    func showAlertToPrivacySettings(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else {
+            print("Something went wrong getting the UIApplicationOpenSettingsURLString")
+            return
+        }
+        let settingsActions = UIAlertAction(title: "Settings", style: .default) { value in
+            UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil )
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(settingsActions)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        handleLocationAuthorizationStatus(status: status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.last
+        print("CURRENT LOCATION IS = \(currentLocation.coordinate.latitude), \(currentLocation.coordinate.longitude)")
+        sortBasedOnSegmentPressed()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location.")
+    }
+}
+
--- Snacktacular/SpotsTableViewCell.swift
+++ Snacktacular/SpotsTableViewCell.swift
@@ -8,6 +8,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SpotsTableViewCell: UITableViewCell {
 
@@ -15,4 +16,17 @@ class SpotsTableViewCell: UITableViewCell {
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
 
+    var currentLocation: CLLocation!
+    var spot: Spot!
+    
+    func configureCell(spot: Spot) {
+        nameLabel.text = spot.name
+        // calculate distance here
+        guard let currentLocation = currentLocation else {
+            return
+        }
+        let distanceInMeters = currentLocation.distance(from: spot.location)
+        let distanceString = "Distance: \((distanceInMeters * 0.00062137).roundTo(places: 2)) miles"
+        distanceLabel.text = distanceString
+    }
 }
